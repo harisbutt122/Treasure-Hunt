3b0abd7c047f1a263476f431c535e0599036ffe8
Harisbutt <harrisbutt122@gmail.com>
2018-09-07 12:58:56 +0500

    commit

---
+++ Treasure Hunt/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  Treasure Hunt
+//
+//  Created by Ammar Waheed on 06/09/2018.
+//  Copyright © 2018 Ammar Waheed. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 5.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setUpView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+
+}
---
+++ Treasure Hunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Treasure Hunt
+//
+//  Created by Ammar Waheed on 06/09/2018.
+//  Copyright © 2018 Ammar Waheed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
